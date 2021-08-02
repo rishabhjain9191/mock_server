7531020bc70ddf56de4f3ab28f5a91aeaed4d426
Rishabh Kumar Jain <rishjain@adobe.com>
2021-08-02 09:57:00 +0530

    refactor add headers

--- app/controllers/mocks_controller.rb
+++ app/controllers/mocks_controller.rb
@@ -2,9 +2,7 @@ class MocksController < ApplicationController
   def handle
     @endpoint = Endpoint.find_by verb: request.method, path: requested_path
     if @endpoint.present?
-      @endpoint.headers.each do |header_key, header_value|
-        response[header_key] = header_value
-      end
+      add_headers
       render json: @endpoint.body, status: @endpoint[:response_code]
     else
       render json: { code: 'not found', "details": 'Requested page ' + requested_path + ' does not exist' },
@@ -13,6 +11,12 @@ class MocksController < ApplicationController
   end
 
   private
+
+  def add_headers
+    @endpoint.headers.each do |header_key, header_value|
+      response[header_key] = header_value
+    end
+  end
 
   def requested_path
     '/' + params[:path]
